28e6eea25fc5b8823175ea823b31db0e0b57d6b4
Bryce Wilbur <bryce.wilbur@my.simpson.edu>
2021-09-18 19:52:42 -0500

    first java project in GitHub

---
+++ economic_projects/src/Bank_Gui/BankTester.java
@@ -0,0 +1,12 @@
+package Bank_Gui;
+
+public class BankTester {
+	public static void main(String[] args) {
+		bank b1 = new bank(4000);
+		b1.setName("Bryce");;
+		
+		//b1.deposit(7564);
+		
+		b1.simpleInterest();
+	}
+}
